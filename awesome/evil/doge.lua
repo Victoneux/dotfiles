5a86002d7b6039806ba6ad1f0a17d445a6719660
Victoneux <victoneux@protonmail.com>
2021-11-19 16:38:40 -0700

    change interval

--- awesome/evil/doge.lua
+++ awesome/evil/doge.lua
@@ -4,7 +4,7 @@
 --      total (integer - mega bytes)
 local awful = require("awful")
 
-local update_interval = 5
+local update_interval = 10
 
 awful.widget.watch("python3 " .. os.getenv("HOME") .. "/.private/info.py", update_interval, function(_, stdout)
     awesome.emit_signal("evil::doge", stdout)
